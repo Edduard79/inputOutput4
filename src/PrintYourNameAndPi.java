bb91ffe31e679d34a9c2acec79c34054b53cb59e
Eduard <eduard.prog22@gmail.com>
2023-01-22 18:21:24 +0100

    First try

---
+++ src/PrintYourNameAndPi.java
@@ -0,0 +1,16 @@
+public class PrintYourNameAndPi {
+
+    public PrintYourNameAndPi(){
+
+    }
+    public static void main(String[] args){
+        String name = "eduard";
+        double pi = 3.141590;
+
+        System.out.printf("%S%nThe number Pi is: %f...\n", name, pi);
+
+
+
+
+    }
+}
